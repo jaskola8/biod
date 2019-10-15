d7e21ff353fa723dd6d9146d8f8893774e5fea26
Bartosz Michałowski <bartoszm.dev@protonmail.com>
2019-10-15 20:19:05 +0200

    Lab 02 TODO.

---
+++ l02.py
@@ -0,0 +1,6 @@
+''' TODO
+- obliczanie entropii maksymalnej ciągu długości k w alfabecie o wielkości n
+- hasło -> klucz
+- AES + CBC file encryption
+- uniwersalna funkcji do bruteforce entropią
+'''
