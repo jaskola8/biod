74545ada53b95da4de8956be093a11f7c048c71b
Jan Starczewski <jan.a.starczewski@gmail.com>
2019-10-15 21:52:16 +0200

    Create function responsible for encoding with aes algorithm in cbc mode

--- l02.py
+++ l02.py
@@ -1,6 +1,38 @@
+from Crypto.Cipher import AES
+import hashlib
+from Crypto.Random import get_random_bytes
+
 ''' TODO
 - obliczanie entropii maksymalnej ciągu długości k w alfabecie o wielkości n
 - hasło -> klucz
 - AES + CBC file encryption
 - uniwersalna funkcji do bruteforce entropią
 '''
+
+chunk_size = 64 * 1024
+encoding = 'utf-8'
+
+
+def main():
+    filename = "./reftexts/letters/110CYL067.txt"
+    print(str(encrypt_with_aes_cbc_mode('Dupa', filename)))
+
+
+def encrypt_with_aes_cbc_mode(key, filename):
+    key = key.encode(encoding=encoding)
+    byte_key = hashlib.sha256(key).digest()
+    iv = get_random_bytes(16)
+    encryptor = AES.new(byte_key, AES.MODE_CBC, iv)
+    with open(filename, 'rb') as file:
+        while True:
+            chunk = file.read(chunk_size)
+            if len(chunk) == 0:
+                break
+            elif len(chunk) % 16 != 0:
+                chunk += bytes(' ' * (16 - len(chunk) % 16), encoding=encoding)
+                by = encryptor.encrypt(chunk)
+                print(by)
+
+
+if __name__ == "__main__":
+    main()
